1915f891a531dccde11ccbeac2a2de5717e3aad1
Roman Berezin <romanberezinua@gmail.com>
2020-06-11 11:09:49 +0300

    Added project

---
+++ DZ_CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DZ_CollectionView
+//
+//  Created by Roman Berezin on 11.06.2020.
+//  Copyright © 2020 Roman Berezin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
